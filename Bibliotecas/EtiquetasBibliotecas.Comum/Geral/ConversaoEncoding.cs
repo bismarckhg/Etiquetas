f6585af370398fa2436a0f62dd2bfdb7a7a04084
Guilherme27383 <guibis672@gmail.com>
2025-09-24 17:47:05 -0300

    Upload

---
+++ Bibliotecas/EtiquetasBibliotecas.Comum/Geral/ConversaoEncoding.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Etiquetas.Bibliotecas.Comum.Geral
+{
+    public static class ConversaoEncoding
+    {
+        public static Encoding UTF8() => new UTF8Encoding(encoderShouldEmitUTF8Identifier: false, throwOnInvalidBytes: true);
+        // Encoding ANSI pt-BR (Windows-1252) com exceção em bytes inválidos
+        public static Encoding Windows1252() => Encoding.GetEncoding
+            (
+                1252,
+                EncoderFallback.ExceptionFallback,
+                DecoderFallback.ExceptionFallback
+            );
+        // UTF-8 sem BOM (recomendado p/ JSON, ZPL etc.)
+        public static Encoding UTF8SemBom() => new UTF8Encoding(encoderShouldEmitUTF8Identifier: false);
+
+
+    }
+}
