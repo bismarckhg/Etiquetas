402c56909e6942da43379434ce1bca3fbfc9920b
Guilherme27383 <guibis672@gmail.com>
2025-12-26 11:27:10 -0300

    Update

--- Domain/Etiquetas.Domain/Configuracao/PosicaoCamposEtiqueta.cs
+++ Domain/Etiquetas.Domain/Configuracao/PosicaoCamposEtiqueta.cs
@@ -1,10 +1,10 @@
-using Etiquetas.Core.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Etiquetas.Core.Interfaces;
 
 namespace Etiquetas.Domain.Configuracao
 {
